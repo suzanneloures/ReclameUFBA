fb971a58f6dab0ae84bb8524dd3c8c1bdc7c5a38
viniciustmacedo <vinymacedo@hotmail.com>
2018-11-25 23:45:23 -0300

    Organizando Projeto

---
+++ ReclameUfba/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = LoginPage;
+
+  pages: Array<{title: string, component: any}>;
+
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+    this.initializeApp();
+
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Feed de Reclamações', component: HomePage },
+      { title: 'Nova Reclamação', component: ListPage },
+      { title: 'Meu Perfil', component: ListPage },
+      { title: 'List', component: ListPage },
+      { title: 'Sair', component: LoginPage }
+    ];
+
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  openPage(page) {
+    this.nav.setRoot(page.component);
+  }
+}
